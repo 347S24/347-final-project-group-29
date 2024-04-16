fa1452a9f59ad43e3d2716d28083a6c509cc912d
Ryan Mendez <mendezrh@dukes.jmu.edu>
2024-04-16 12:08:54 -0400

    updated

--- group_29_final/group29final/group29final/peer_instruction/views.py
+++ group_29_final/group29final/group29final/peer_instruction/views.py
@@ -1,21 +1,28 @@
 from django.views import generic
+from django.views.generic import ListView
 from django.shortcuts import render, redirect
 from peer_instruction.models import Question
 from peer_instruction.forms import QuestionForm
 
 
-def QuestionList(generic.ListView):
-    def get(self, request, *args, **kwargs):
-        questions = Question.objects.all()    
-        return render(request, 'teacher_home.html', {'questions': questions})
-    
+def teacher_home(request):
+    questions = Question.objects.all()
+    return render(request, 'teacher_home.html', {'questions': questions})
+
+
+# def QuestionListView(generic.ListView):
+#     model = Question
+
 
 def add_question(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
+            print('valid form')
             form.save()
             return redirect('teacher_home')
+        else:
+            print('imnvalid form?')
     else:
         form = QuestionForm()
     return render(request, 'add_question.html', {'form': form})
