5726fbe3ac9e9c22d7c12c01abf6e5c5a5a9aa4c
Ryan Mendez <mendezrh@dukes.jmu.edu>
2024-04-25 11:16:11 -0400

    fixed homepage

--- group_29_final/group29final/config/urls.py
+++ group_29_final/group29final/config/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     # Your stuff: custom urls includes go here
     path('home', views.teacher_home, name='teacher_home'),
     path('add/', views.add_question, name='add_question'),
+    path("peer-instruction/", include("group29final.peer_instruction.urls")),
+
     # path('question/<int:question_id>/', views.question_detail, name='question_detail'),
     # path('question/<int:question_id>/submit/', views.student_answer_submission, name='student_answer_submission'),
     # ...
--- group_29_final/group29final/group29final/peer_instruction/urls.py
+++ group_29_final/group29final/group29final/peer_instruction/urls.py
@@ -1,7 +1,7 @@
 # In urls.py
 
 from django.urls import path
-from ... import views
+from . import views 
 
 urlpatterns = [
     path('home/', views.teacher_home, name='teacher_home'),
