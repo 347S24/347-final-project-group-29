e9e8e80205f03baa263c3728e9a1b5878525e3c6
Ryan Mendez <mendezrh@dukes.jmu.edu>
2024-05-08 15:28:07 -0400

    updated

--- group_29_final/group29final/group29final/peer_instruction/urls.py
+++ group_29_final/group29final/group29final/peer_instruction/urls.py
@@ -1,7 +1,8 @@
 # In urls.py
 
 from django.urls import path
-from . import views 
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('home/', views.teacher_home, name='teacher_home'),
@@ -13,4 +14,7 @@ urlpatterns = [
     path('question/delete/<int:question_id>/', views.delete_question, name='delete_question'),
     path('thanks/', views.thanks_page, name='thanks_page'),
     path('clear_answers/<int:question_id>/', views.clear_answers, name='clear_answers'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- group_29_final/group29final/group29final/peer_instruction/views.py
+++ group_29_final/group29final/group29final/peer_instruction/views.py
@@ -12,6 +12,10 @@ from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from PIL import Image
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def teacher_home(request):
@@ -84,3 +88,24 @@ def clear_answers(request, question_id):
         # For example, to clear all answers:
         Answer.objects.filter(question_id=question_id).delete()
     return redirect('view_answers', question_id=question_id)
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('teacher_home')
+#     else:
+#         form = AuthenticationForm()
+#     return render(request, 'login.html', {'form': form})
