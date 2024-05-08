24df83f9413cc50cf30433c80c0c606df33c4666
Ryan Mendez <mendezrh@dukes.jmu.edu>
2024-05-08 13:43:40 -0400

    up

--- group_29_final/group29final/config/settings/production.py
+++ group_29_final/group29final/config/settings/production.py
@@ -10,7 +10,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["ryanmproject.rocks"])
-ALLOWED_HOSTS += ["142.93.64.74"]
+ALLOWED_HOSTS += ["142.93.64.74", "localhost"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
